7d7bb3e9eca5a5c61f75950c7d867ca7df566176
Yash Kalavadiya <yashkalavadiya1010@gmail.com>
2022-05-04 12:49:07 +0530

    conflicts resolved

--- test.js
+++ test.js
@@ -9,3 +9,15 @@ init({
 console.log(await getFile("/abcd", "file6"));
 
 //TODO: fix typos, text format of file with MIME type
+
+// console.log("Data: ", arrBuf);
+// let bs64 = new Uint8Array(arrBuf);
+// const reduces = bs64.reduce((data, byte) => {
+//     return data + String.fromCharCode(byte);
+// });
+
+// console.log(JSON.parse(data.headers)[["content-type"]]);
+// const tempData = await data.text();
+// const file = Buffer.toString()
+// console.log(tempData);
+// return data.text();
